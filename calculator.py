369d666d22d24add192a5d2e2840214bcad7cfc2
William Tran <wtran29@gmail.com>
2019-09-11 12:47:34 -0700

    added divide function and test

--- calculator.py
+++ calculator.py
@@ -8,3 +8,8 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+
+    return first_term / second_term
